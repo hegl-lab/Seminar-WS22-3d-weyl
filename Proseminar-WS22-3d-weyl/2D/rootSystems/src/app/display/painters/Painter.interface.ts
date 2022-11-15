6bb8b4e04c9405514eb2360ceee5294604a3d132
ricardowaibel <ricardowaibel@web.de>
2022-11-15 15:56:37 +0100

    Delete unnecessary directory

--- Proseminar-WS22-3d-weyl/2D/rootSystems/src/app/display/painters/Painter.interface.ts
+++
@@ -1,6 +0,0 @@
-import { PaintLayer } from "src/app/services/paint.service";
-
-// Interface to determine the api for painters
-export default interface Painter{
-    paint(layer?: PaintLayer): void;
-}
